f5c3098edd9e99965151e18fb1b62a14a05d967c
nikk151 <nikhilparihar151@gmail.com>
2026-03-14 14:09:26 +0530

    made gitDiff function and made global command

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log("offline-pr-gen CLI is alive!")
+const { gitDiff } = require('./git')
+const version = require('../package.json').version
+const { program } = require('commander')
+
+program.name('offline-pr-gen').description('CLI for generating PRs').version(version)
+program.option('-s, --staged', 'Read only the staged git changes').option('-o, --output <output>', 'takes file for generation').option('-m, --model <model_name>', 'Model to use for generation', 'qwen2.5-coder:1.5b')
+program.parse(process.argv)
+
+const options = program.opts()
+console.log(gitDiff(options.staged))
+
+console.log(options)
---
+++ src/git.js
@@ -0,0 +1,17 @@
+const {execSync}= require('child_process')
+
+
+function gitDiff(staged){
+    try{
+
+        if(staged){
+            return execSync('git diff --staged', { encoding: 'utf-8' , stdio: 'pipe'})
+        }
+        return execSync('git diff', { encoding: 'utf-8' , stdio: 'pipe'})
+    }catch(error){
+        console.log("Error: This does not appear to be a git repository.")
+        process.exit(1)
+    }
+}
+
+module.exports = {gitDiff}
